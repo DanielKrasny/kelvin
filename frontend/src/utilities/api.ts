84e0e23661a5acf4ada6ec722341406227c2f97a
Daniel Krásný <53856821+DanielKrasny@users.noreply.github.com>
2026-04-19 15:33:11 +0200

    feat: Show error messages from API in toast
    In case it is enabled in `getFromAPI()` / `getDataWithCSRF()` (disabled by default), an API call which will result in error and will contain `detail` key (common for v2 endpoints) will display this error to user as toast.
    
    I also added an interface for paginated responses from v2 API, as the response format is a standard in Django Ninja pagination.

--- frontend/src/utilities/api.ts
+++ frontend/src/utilities/api.ts
@@ -1,5 +1,12 @@
+import { toastApi } from './toast';
+
 export function csrfToken() {
     return document.querySelector('meta[name=csrf-token]').getAttribute('content');
+}
+
+export interface PaginatedResponse<T> {
+    items: T[];
+    count: number;
 }
 
 type Method = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
@@ -19,7 +26,8 @@ export const getFromAPI = async <$ReturnType>(
     url: string,
     method?: Method,
     data?: unknown,
-    headers?: HeadersInit
+    headers?: HeadersInit,
+    showErrorToast = false
 ): Promise<$ReturnType | undefined> => {
     try {
         const response = await fetch(url, {
@@ -28,11 +36,13 @@ export const getFromAPI = async <$ReturnType>(
             body: data ? JSON.stringify(data) : undefined
         });
 
+        const json = await response.json();
         if (!response.ok) {
+            if (json?.detail && showErrorToast) {
+                toastApi.error(json.detail);
+            }
             return undefined;
         }
-
-        const json = await response.json();
         return json as $ReturnType;
     } catch (error) {
         console.error(error);
@@ -46,6 +56,7 @@ export const getFromAPI = async <$ReturnType>(
  * @param url url to fetch
  * @param data data which will be sent to the API
  * @param method HTTP method, if you want to override the default GET/POST
+ * @param showErrorToast Whether to show an error toast on failure
  * @note If data is passed, the request will be a POST request, otherwise a GET request, if not overridden by method parameter
  * @param headers Headers for the request
  *
@@ -55,10 +66,11 @@ export const getDataWithCSRF = async <$ReturnType>(
     url: string,
     method?: Method,
     data?: unknown,
-    headers?: HeadersInit
+    headers?: HeadersInit,
+    showErrorToast = false
 ): Promise<$ReturnType | undefined> => {
     const CSRF = {
         'X-CSRFToken': csrfToken()
     };
-    return getFromAPI(url, method, data, headers ? { ...headers, ...CSRF } : CSRF);
+    return getFromAPI(url, method, data, headers ? { ...headers, ...CSRF } : CSRF, showErrorToast);
 };
