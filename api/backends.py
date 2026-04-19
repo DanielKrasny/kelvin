3cb1e042407dae1aac6c9faec1934299e613b4aa
Daniel Krásný <53856821+DanielKrasny@users.noreply.github.com>
2026-04-19 13:43:02 +0200

    fix: Disallow creating new API tokens without session auth
    When an API token is used to access the API (instead of session cookie for the web client), we shouldn't allow to generate new API tokens this way.

--- api/backends.py
+++ api/backends.py
@@ -13,7 +13,10 @@ class TokenBackend(ModelBackend):
     def authenticate(self, request, token=None):
         if token:
             try:
-                return get_user_model().objects.get(usertoken__token=hash_token(token))
+                token_secured = hash_token(token)
+                user = get_user_model().objects.get(usertoken__token=token_secured)
+                user.current_token = token_secured
+                return user
             except ObjectDoesNotExist:
                 pass
         return None
