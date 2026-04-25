f8e0f9e4fab306b73f39e113b8f05433a415ad14
Daniel Krásný <53856821+DanielKrasny@users.noreply.github.com>
2026-04-25 16:31:05 +0200

    chore: Improve Django Admin listing of attendance devices

--- attendance/admin.py
+++ attendance/admin.py
@@ -3,6 +3,10 @@ from .models import AttendanceDevice, ClassSession
 
 
 class AttendanceDeviceAdmin(admin.ModelAdmin):
+    list_display = ("device_name", "user", "state", "created_at")
+    search_fields = ("user__username",)
+    search_help_text = "Search by login of the device owner"
+
     def has_add_permission(self, request):
         # Since public key cannot be added via Web UI, the admin shouldn't create new devices manually
         return False
