db2d74223bdb15ba103718da7f70df906b9a07cf
Daniel Krásný <53856821+DanielKrasny@users.noreply.github.com>
2026-04-26 00:31:18 +0200

    feat: Add endpoints for class sessions

--- api/v2/attendance/__init__.py
+++ api/v2/attendance/__init__.py
@@ -1,5 +1,7 @@
 from ninja import Router
+from .class_session.default import router as class_session_default_router
 from .device.default import router as device_default_router
 
 router = Router(tags=["Attendance"])
+router.add_router("/class-session", class_session_default_router)
 router.add_router("/device", device_default_router)
