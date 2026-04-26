41790d1d1d0e10f6040805d5d6716034837226a7
Daniel Krásný <53856821+DanielKrasny@users.noreply.github.com>
2026-04-26 22:14:34 +0200

    feat: Add endpoints for attendance recording

--- api/v2/attendance/dto.py
+++ api/v2/attendance/dto.py
@@ -1,8 +1,9 @@
 import base64
 from datetime import datetime
+from typing import List, Dict
 from ninja import Schema
 from pydantic import Field, model_validator, PrivateAttr
-from attendance.models import AttendanceToken
+from attendance.models import AttendanceToken, Attendance
 
 
 class AttendancePublicKeyDTO(Schema):
@@ -44,6 +45,10 @@ class EncryptedMessageSchema(Schema):
         return self
 
 
+class EncryptedMessageReceivedSchema(EncryptedMessageSchema):
+    received_at: datetime = Field(..., description="Date of received attendance recording")
+
+
 class EncryptedMessageInnerSchema(Schema):
     data: str = Field(..., description="Original data string that was signed")
     signature: str = Field(..., description="Base64-encoded signature of the data string")
@@ -72,6 +77,33 @@ class CreateAttendanceTokenInnerSchema(EncryptedMessageDataBaseSchema):
     class_session_id: int = Field(..., description="ID of the class session this token is for")
 
 
+class CreateAttendanceRecordStudentInnerSchema(EncryptedMessageDataBaseSchema):
+    class_session_id: int = Field(..., description="ID of the class session this token is for")
+    token: str = Field(..., description="Token of attendance recording")
+    created_at: datetime = Field(..., description="Date of attendance recording creation")
+
+
+class CreateAttendanceRecordsTeacherInnerSchema(EncryptedMessageDataBaseSchema):
+    class_session_id: int = Field(..., description="ID of the class session")
+    records: List[EncryptedMessageReceivedSchema] = Field(
+        ..., description="Encrypted records of student attendance with time of receipt"
+    )
+
+
+class CreateAttendanceRecordTeacherManualSchema(Schema):
+    student_login: str = Field(..., description="Student login")
+    is_present: bool = Field(
+        ..., description="Whether student was marked as present in the class session"
+    )
+
+
+class CreateAttendanceRecordsTeacherManualSchema(Schema):
+    class_session_id: int = Field(..., description="ID of the class session")
+    records: List[CreateAttendanceRecordTeacherManualSchema] = Field(
+        ..., description="Records of student attendance to be created manually by teacher"
+    )
+
+
 class AttendanceTokenDTO(Schema):
     id: int = Field(..., description="ID of attendance token")
     token: str = Field(..., description="Attendance token")
@@ -79,6 +111,29 @@ class AttendanceTokenDTO(Schema):
     created_at: datetime = Field(..., description="Date of attendance token creation")
     expires_at: datetime = Field(..., description="Date of attendance token expiration")
     class_session_id: int = Field(..., description="ID of the class session this token is for")
+
+
+class AttendanceRecordDTO(Schema):
+    id: int = Field(..., description="ID of attendance record")
+    student_login: str = Field(..., description="Student login")
+    class_session_id: int = Field(
+        ..., description="ID of the class session for this attendance record"
+    )
+    attendance_time: datetime = Field(..., description="Time of attendance recording")
+    is_present: bool = Field(
+        ..., description="Whether the student was marked as present in the class session"
+    )
+    record_format: Attendance.RecordFormat = Field(..., description="Attendance record format")
+    created_at: datetime = Field(..., description="Date of attendance record creation")
+    updated_at: datetime = Field(..., description="Date of attendance record last update")
+    created_by_login: str = Field(
+        ..., description="Login of user who created this attendance record"
+    )
+
+
+class CreateAttendanceRecordsTeacherResponseSchema(Schema):
+    success: List[AttendanceRecordDTO] = Field(..., description="Successfully created records")
+    failed: Dict[int, str] = Field(..., description="Failed records with error messages")
 
 
 def attendance_token_to_dto(token: AttendanceToken) -> AttendanceTokenDTO:
@@ -89,4 +144,18 @@ def attendance_token_to_dto(token: AttendanceToken) -> AttendanceTokenDTO:
         created_at=token.created_at,
         expires_at=token.expires_at,
         class_session_id=token.class_session_id,
+    )
+
+
+def attendance_record_to_dto(record: Attendance) -> AttendanceRecordDTO:
+    return AttendanceRecordDTO(
+        id=record.pk,
+        student_login=record.student.username,
+        class_session_id=record.class_session_id,
+        attendance_time=record.attendance_time,
+        is_present=record.is_present,
+        record_format=Attendance.RecordFormat(record.record_format),
+        created_at=record.created_at,
+        updated_at=record.updated_at,
+        created_by_login=record.created_by.username,
     )
