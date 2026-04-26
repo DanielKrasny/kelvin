e63b1c3aca85d3e7f35cd5189c5957ce312324aa
Daniel Krásný <53856821+DanielKrasny@users.noreply.github.com>
2026-04-27 00:16:59 +0200

    feat: Show attendance records in Django admin

--- attendance/admin.py
+++ attendance/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import AttendanceDevice, ClassSession
+from .models import AttendanceDevice, ClassSession, Attendance
 
 
 class AttendanceDeviceAdmin(admin.ModelAdmin):
@@ -12,5 +12,12 @@ class AttendanceDeviceAdmin(admin.ModelAdmin):
         return False
 
 
+class AttendanceRecordAdmin(admin.ModelAdmin):
+    list_display = ("student", "class_session", "attendance_time", "is_present", "record_format")
+    search_fields = ("student__username",)
+    search_help_text = "Search by login of the student"
+
+
 admin.site.register(AttendanceDevice, AttendanceDeviceAdmin)
 admin.site.register(ClassSession)
+admin.site.register(Attendance, AttendanceRecordAdmin)
