e749be7e5ec55c2fbccd72e0d2870773d04e3eaf
Daniel Krásný <53856821+DanielKrasny@users.noreply.github.com>
2026-04-26 22:31:15 +0200

    feat: Add upcoming class sessions endpoint

--- api/v2/attendance/class_session/dto.py
+++ api/v2/attendance/class_session/dto.py
@@ -12,6 +12,7 @@ class ClassSessionDTO(Schema):
     class_code: str = Field(..., description="Code of the related class")
     class_day: Class.Day = Field(..., description="Day of the related class (e.g., PO, UT, ...)")
     class_time: str = Field(..., description="Time of the related class (HH:MM)")
+    teacher_id: int = Field(..., description="ID of the teacher for the class")
     start: datetime = Field(..., description="Start time of the class session")
     end: datetime = Field(..., description="End time of the class session")
     created_at: datetime = Field(..., description="Creation time of the class session")
@@ -29,6 +30,7 @@ def class_session_to_dto(session: ClassSession) -> ClassSessionDTO:
         class_code=session.clazz.code,
         class_day=session.clazz.day,
         class_time=session.clazz.time.strftime("%H:%M"),
+        teacher_id=session.clazz.teacher.pk,
         start=session.start,
         end=session.end,
         created_at=session.created_at,
